3a865cb1b0242e1b0901bf5ce3f46986b7f0b655
Lucas Rosa <lucas@telnyx.com>
2023-01-12 21:06:47 -0300

    keep consistency

--- allowlist_bypasses/jsonp.ts
+++ allowlist_bypasses/jsonp.ts
@@ -132,7 +132,7 @@ export const URLS: string[] = [
   '//autocomplete.travelpayouts.com/avia',
   '//www.googleapis.com/freebase/v1/topic/m/0344_',
   '//mts1.googleapis.com/mapslt/ft',
-  "//publish.twitter.com/oembed",
+  '//publish.twitter.com/oembed',
   '//fast.wistia.com/embed/medias/o75jtw7654.json',
   '//partner.googleadservices.com/gampad/ads',
   '//pass.yandex.ru/services',
